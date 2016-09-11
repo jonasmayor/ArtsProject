19f3b877cb9682dbbe4556754467f33436ba140c
Aneli Valdes Acosta <anelivaldes@gmail.com>
2016-09-11 11:12:27 -0400

    partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
+
   def index
     if params[:set_locale]
       redirect_to home_path(locale: params[:set_locale])
     end
+    @links=[{:title => 'Colecciones', :href => '#collections'},
+            {:title => 'Nuevos Productos', :href => '#new_products'},
+            {:title => 'Artistas más vendidos', :href => '#most_selled'},
+            {:title => 'Especiales', :href => '#specials'}]
+  end
+
+  private
+  def load_example_data
+    @product=[]
   end
 end
