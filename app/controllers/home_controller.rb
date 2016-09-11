32fc264b54a15afe8abd058cfd2b763477555356
Aneli Valdes Acosta <anelivaldes@gmail.com>
2016-09-11 19:01:56 -0400

    main and footer responsive

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,16 @@ class HomeController < ApplicationController
     if params[:set_locale]
       redirect_to home_path(locale: params[:set_locale])
     end
+
     @links=[{:title => 'Colecciones', :href => '#collections'},
             {:title => 'Nuevos Productos', :href => '#new_products'},
             {:title => 'Artistas más vendidos', :href => '#most_selled'},
             {:title => 'Especiales', :href => '#specials'}]
+
   end
 
-  private
   def load_example_data
     @product=[]
   end
+
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+  def get_next_link
+    @links.push @links[0]
+    @links.delete_at 0
+  end
 end
